edbd3d72a20904971fa4bc9af76e03d999c1854b
aikudeshu <yangxiuling1949@163.com>
2022-11-10 22:44:56 +1300

    Step1 to Step2 Create a simple string calculator that takes strings of numbers and return their sum

--- src/test/TestStringCalculator.java
+++ src/test/TestStringCalculator.java
@@ -1,4 +1,40 @@
 package test;
 
+import main.java.com.nsTest.StringCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestStringCalculator {
+    @Test
+    public void testAddEmpty(){
+        //1-1
+        StringCalculator strCalculator = new StringCalculator();
+        int result = strCalculator.add("");
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void testAddOneNum(){
+        //1-2
+        StringCalculator strCalculator = new StringCalculator();
+        int result = strCalculator.add("1");
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void testAddTwoNum(){
+        //1-3
+        StringCalculator strCalculator = new StringCalculator();
+        int result = strCalculator.add("1,2");
+        assertEquals(3,result);
+    }
+
+    @Test
+    public void testAddUnknownAmountOfNum(){
+        //2 Adjust the add method to handle an unknown amount of numbers
+        StringCalculator strCalculator = new StringCalculator();
+        int result = strCalculator.add("1,2,3,4");
+        assertEquals(10,result);
+    }
 }
